2375aff3403b2ab4775c5f57a9562b42be33aec9
jrpaarlberg <jason.paarlberg@gmail.com>
2013-11-18 22:10:47 -0500

    Adding bogus String

--- ConvergeReports/SalesCommission.aspx.vb
+++ ConvergeReports/SalesCommission.aspx.vb
@@ -4,7 +4,7 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Dim instanceReportSource As New Telerik.Reporting.InstanceReportSource()
         instanceReportSource.ReportDocument = New ClassSalesCommission.Report1()
-
+		Dim strJason as String 
         ReportViewer1.ReportSource = instanceReportSource
 
         ReportViewer1.RefreshReport()
